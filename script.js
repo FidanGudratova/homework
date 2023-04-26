e0bd262c48239dd84bb7c95b66305645d318ba9a
EvoCoding <hseynli@list.ru>
2023-04-26 16:33:09 +0400

    2

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 //1
-let num = +prompt('eded daxil edin');
-let isEven = num % 2 === 0 ? 'true' : 'false';
-console.log(isEven);
+// let num = +prompt('eded daxil edin');
+// function isEven(number) {
+//     let isEven = number % 2 === 0 ? 'true' : 'false';
+//     console.log(isEven);
+// }
+// isEven(num);
+//2
+// let num = +prompt('eded');
+// function fourth(num) {
+//     let fourth = num **4 ;
+//     console.log(fourth);
+// }
+// fourth(num);
+//3
+let phoneNumber = +prompt('entered 11 number');
+function isvalidnumber(item) {
+     let number = item.length === 13 && '\+ \7 +number' ? 'true' : 'false' ;
+
+    }
+
+isvalidnumber(phoneNumber);
+
+
